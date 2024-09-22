ddef46a6e62002d8273944a2305d299cf4cc47d3
kienvtm <kienvtm@gmail.com>
2024-09-22 13:23:11 +0700

    change layout

--- app.py
+++ app.py
@@ -44,7 +44,8 @@ with col1:
 with col2:
     player2 = st.text_input(label='Nhap ten Player 2', placeholder ='Player 2 name', value ='Player 2')
 
-with st.container(border=True):
+# with st.container(border=True):
+with col1:
     with stylable_container(
             key="green_button",
             css_styles="""
@@ -56,8 +57,9 @@ with st.container(border=True):
                 """,
         ):
         col11, col12, col13 = st.columns(3)
-        col31, col32, col33 = st.columns(3)
+        # col31, col32, col33 = st.columns(3)
         col21, col22, col23 = st.columns(3)
+        # col11, col12 = st.columns(2)
         with col11:
             st.button('+1', on_click=increment_counter1,
                 kwargs=dict(increment_value=1))
@@ -68,9 +70,10 @@ with st.container(border=True):
             st.button('+3', on_click=increment_counter1,
                 kwargs=dict(increment_value=3))
         
-        with col32:
-            st.metric(label=player1, value=st.session_state.count1)
+        # with col32:
+        st.metric(label=player1, value=st.session_state.count1)
         
+        # with col12:
         with col21:
             st.button('+9', on_click=increment_counter1,
                 kwargs=dict(increment_value=9))
@@ -81,7 +84,8 @@ with st.container(border=True):
             st.button('-2', on_click=decrement_counter1,
                 kwargs=dict(decrement_value=2))
             
-with st.container(border=True):
+# with st.container(border=True):
+with col2:
     with stylable_container(
             key="orange_button",
             css_styles="""
@@ -103,11 +107,10 @@ with st.container(border=True):
             st.button('+3', key='button23', on_click=increment_counter2,
                 kwargs=dict(increment_value=3))
         
-        # with col13:
-        col31, col32, col33 = st.columns(3)
+        # # with col13:
+        # col31, col32, col33 = st.columns(3)
             
-        with col32:
-            st.metric(label=player2, value=st.session_state.count2)
+        # with col32:
         # fig = go.Figure()
         # fig.add_trace(go.Indicator(
         #         mode = "number",
@@ -127,3 +130,5 @@ with st.container(border=True):
         with col23:
             st.button('-2', key='button26', on_click=decrement_counter2,
                 kwargs=dict(decrement_value=2))
+        
+        st.metric(label=player2, value=st.session_state.count2)
