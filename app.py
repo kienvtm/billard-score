7e2d02eb24fc12486cb44cae759b65f9b435dc5f
kienvtm <kienvtm@gmail.com>
2024-09-22 20:52:30 +0700

    change secret key for googlesheet

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ def update_data(worksheet, df):
     first_empty_row = len(str_list) + 1
     set_with_dataframe(worksheet, df, row=first_empty_row, col=1, include_index=False, include_column_header=False)
 
-key = st.secrets["connections.gsheets"]
+key = st.secrets["connections.gsheets"]['spreadsheet']
 
 sheet_name='Sheet1'
 if st.button("End Game"):
